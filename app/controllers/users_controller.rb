39296ac260d696d55fd964e732df2f547197690d
Olha Bezpalchuk <olha.bezpalchuk@gmail.com>
2017-03-04 02:26:33 +0200

    Registration and Log In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   def new
@@ -10,14 +25,25 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
+
+    respond_to do |format|
+      if @user.save
+        log_in @user
+        format.html { redirect_to @user }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
+
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:login, :name, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   has_many :words
 
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :login, presence: true, length: { maximum: 50 },
+                    uniqueness: { case_sensitive: true }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
   has_secure_password
 end
