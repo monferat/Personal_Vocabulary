f728dc97aa272135fc8c09affa714129c6219436
Olha Bezpalchuk <olha.bezpalchuk@gmail.com>
2017-04-17 18:08:39 +0300

    Added validations for theme and word

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,7 @@
 class Theme < ApplicationRecord
   has_many :words
+
+  validates :name, presence: true, length: { maximum: 50 },
+            uniqueness: { case_sensitive: false }
+
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,8 @@
 class Word < ApplicationRecord
   belongs_to :theme
   has_many :user_words
+
+  validates :name, presence: true, length: { maximum: 50 },
+            uniqueness: { case_sensitive: false }
+
 end
